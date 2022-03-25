db0ab8df40ad2edcd1b1e17431f9b24edf7e4c94
Umut Oku <umut.oku@gmail.com>
2022-03-25 17:01:56 +0300

    3ve4 bölünme ödevi eklendi

---
+++ JavaPatika/src/Pratik/Deneme.java
@@ -0,0 +1,23 @@
+package Pratik;
+
+import java.util.Scanner;
+
+public class Deneme {
+    public static void main(String[] args) {
+       Scanner input = new Scanner(System.in);
+       int sayi,counter=0,toplam=0;
+        System.out.print("Sayı girin: ");
+        sayi=input.nextInt();
+       for(int i=1;i<=sayi;i++)
+       {
+
+           if(i%3==0&&i%4==0)
+           {
+               toplam+=i;
+               counter++;
+           }
+       }
+       System.out.println("3 ve 4'e kalansız bölünen sayılar: "+ toplam/counter);
+
+    }
+}
